adf3db294c12240e575791d812e0bbeef071ec1c
Duk3lo <jhosue10avecillas@gmail.com>
2026-07-16 00:01:08 -0500

    update

--- firmware/src/hardware/pins.rs
+++ firmware/src/hardware/pins.rs
@@ -2,7 +2,7 @@ use esp_idf_svc::hal::gpio::AnyIOPin;
 
 
 pub fn is_safe_gpio(pin_num: u32) -> bool {
-    matches!(pin_num, 0 | 2 | 4 | 5 | 12..=19 | 21..=23 | 25..=27 | 32 | 33);
+    matches!(pin_num, 0 | 2 | 4 | 5 | 12..=19 | 21..=23 | 25..=27 | 32 | 33)
 }
 
 pub fn get_any_pin(pin_num: u32) -> Option<AnyIOPin<'static>> {
